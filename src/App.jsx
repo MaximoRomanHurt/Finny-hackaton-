a9a7a9a7ff63476cca8ad5ef31c9f875051297a8
MaximoRomanHurt <maximo.roman@unmsm.edu.pe>
2025-11-16 20:22:18 -0500

    6

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <div className="login-card">
         <div className="login-header">
           <div className="logo">
-            <img src="/logo.jpge" alt="Finny Logo" />
+            <img src="/logo.png" alt="Finny Logo" />
           </div>
           <h1>Finny</h1>
           <p>Controla tus gastos de forma inteligente</p>
